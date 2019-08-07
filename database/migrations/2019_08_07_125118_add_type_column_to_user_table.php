4f75d42fe7569a48f4c318d591e0be9242397fe6
Tobias Markus <tobbi.bugs@googlemail.com>
2019-08-07 15:38:49 +0200

    Add default of 0 for role

--- database/migrations/2019_08_07_125118_add_type_column_to_user_table.php
+++ database/migrations/2019_08_07_125118_add_type_column_to_user_table.php
@@ -14,7 +14,7 @@ class AddTypeColumnToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role')->unsigned();
+            $table->integer('role')->unsigned()->default(0);
         });
     }
 
