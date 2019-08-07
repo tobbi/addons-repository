047865804d900470eea60333640c18ad7d4cd741
Tobias Markus <tobbi.bugs@googlemail.com>
2019-08-07 16:13:02 +0200

    Remove search bar at the top

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -35,6 +35,10 @@
             <a class="nav-link disabled" href="#">Disabled</a>
         </li> --}}
         </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
          <!-- Right Side Of Navbar -->
          <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
@@ -67,9 +71,5 @@
                 </li>
             @endguest
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
 </nav>
