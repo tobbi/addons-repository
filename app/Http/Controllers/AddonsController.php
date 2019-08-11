c08bcfd9decebc0a3b21b428cedf5861a8d29182
Tobias Markus <tobbi.bugs@googlemail.com>
2019-08-11 22:06:49 +0200

    Semi-working parser code

--- app/Http/Controllers/AddonsController.php
+++ app/Http/Controllers/AddonsController.php
@@ -46,8 +46,20 @@ class AddonsController extends Controller
         // AddonsController::ShowAll();
     }
 
+    /**
+     * Parses one block of add-ons in the nfo file
+     * and adds it to the database.
+     */
     private function _parseAddonInfo(array $addonInfo)
     {
+        $id = null;
+        $kind = null;
+        $title = null;
+        $author = null;
+        $license = null;
+        $httpUrl = null;
+        $file = null;
+        $md5 = null;
         foreach($addonInfo as $property)
         {
             $type = gettype($property);
@@ -58,10 +70,52 @@ class AddonsController extends Controller
                 break;
 
                 case "array":
-                    echo $property[0]."<br/>"; // key
+                    $key = $property[0];
+                    $value = $property[1];
+                    switch($key)
+                    {
+                        case "kind":
+                        $kind = ($value == "Worldmap") ? 1 : 2;
+                        break;
+                        case "title":
+                        $title = $value;
+                        break;
+                        case "author":
+                        $author = $value;
+                        break;
+                        case "license":
+                        $license = $value;
+                        break;
+                        case "http-url":
+                        $httpUrl = $value;
+                        break;
+                        case "file":
+                        $file = $value;
+                        break;
+                        case "md5":
+                        $md5 = $value;
+                        break;
+
+                        default:
+                            echo "Unknown value: ".$key;
+                        break;
+                    }
                 break;
             }
         }
+        $addon = new Addon();
+        $addon->title = $title;
+        $addon->version = 0.1;
+        $addon->description = $title;
+        $addon->slug = "addon-slug-tbd";
+        $addon->http_url = $httpUrl;
+        $addon->thumb_url = $httpUrl;
+        $addon->md5 = $md5;
+        $addon->author_id = 1;
+        $addon->license_id = 1;
+        $addon->enabled = true;
+        $addon->type = $kind;
+        $addon->save();
     }
 
     public function MigrateFromNFO(Request $request)
