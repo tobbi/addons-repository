04491c5cfa1482ea012c3fcebd77fe4e9890e97a
Tobias Markus <tobbi.bugs@googlemail.com>
2019-08-16 12:48:01 +0200

    Use input type URL for nfo URL

--- resources/views/listing/migrate.blade.php
+++ resources/views/listing/migrate.blade.php
@@ -57,7 +57,7 @@ function performGETRequest($caller)
 <div class="form-group row">
     <label for="nfoURL" class="col-sm-2 col-form-label">.nfo URL</label>
     <div class="col-sm-10">
-    <input type="text" class="form-control" id="nfoURL" placeholder="http://www.example.com/v0.5.0.nfo" name="nfoURL">
+    <input type="url" class="form-control" id="nfoURL" placeholder="http://www.example.com/v0.5.0.nfo" name="nfoURL">
     </div>
     <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
   </div>
