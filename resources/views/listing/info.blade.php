a07213680a19b2ddc20367ec717f1761fad28cd7
Tobias Markus <tobbi.bugs@googlemail.com>
2019-08-19 13:24:10 +0200

    Message when add-on is disabled on info page

--- resources/views/listing/info.blade.php
+++ resources/views/listing/info.blade.php
@@ -3,6 +3,10 @@
 @section('title', $addon->title)
 
 @section('content')
+@if(!$addon->enabled)
+<p>The specified add-on is disabled.</p>
+@else
 <h2>{{ $addon->title }}</h2>
 <h3>by {{ $addon->author->name }}<h3>
+@endif
 @endsection
